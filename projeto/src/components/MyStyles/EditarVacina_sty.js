cd6a75251f34f1d4ed6d4b643797a4d8df7bf828
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-05 17:51:11 -0300

    Modal feito e Layout de EditarVacina refatorado.

--- projeto/src/components/MyStyles/EditarVacina_sty.js
+++ projeto/src/components/MyStyles/EditarVacina_sty.js
@@ -4,16 +4,19 @@ const EditarVacina_sty = StyleSheet.create({
     container: {
 
         containerView: {
+            flex: 1,
             flexDirection: 'column',
             alignItems: 'center',
+            justifyContent: 'center',
             backgroundColor: '#ADD4D0',
-            width: '100%',
             height: '100%',
-            gap: 8,
+            width: '95%',
+            gap: 13,
             marginTop: 65,
         },
 
         containerKeyboard: {
+            flex: 1,
             backgroundColor: '#ADD4D0',
             width: '100%',
             height: '100%',
@@ -22,18 +25,20 @@ const EditarVacina_sty = StyleSheet.create({
     },
 
     containerInputs: {
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'flex-start',
-        width: '100%',
-        marginLeft: 30,
+        justifyContent: 'center',
+        gap: 10,
+        //    marginLeft: 30,
     },
 
     containerRadioButton: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        flexWrap: 'wrap',
+        gap: 5,
     },
 
     text: {
@@ -46,48 +51,46 @@ const EditarVacina_sty = StyleSheet.create({
 
     },
 
-
-    textRadioButton: {
-        color: '#FFFFFF',
-        fontSize: 14,
-        fontFamily: 'AveriaLibre-Regular',
-    },
-
     inputs: {
         flex: 60,
         width: '100%',
         fontSize: 16,
-        height: 30,
+        height: 35,
         backgroundColor: '#FFFFFF',
         fontFamily: 'AveriaLibre-Regular',
-        paddingRight: 10,
-        marginRight: 50,
-        marginLeft: 10,
+        //   paddingRight: 10,
+        //   marginRight: 50,
+        //    marginLeft: 10,
         color: '#419ED7',
         alignSelf: 'center',
         paddingVertical: 5,
+        //     paddingVertical: 5,
     },
 
     buttons: {
         buttonContainer: {
             flex: 65,
             flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 8,
             width: '100%',
-            marginRight: 50,
-            gap: -20,
+            //    marginRight: 50,
+            //    gap: -20,
         },
 
         textButton: {
             color: '#FFFFFF',
-            fontSize: 17,
+            fontSize: 16,
             fontFamily: 'AveriaLibre-Regular',
         },
 
         button1: {
-            width: '94%',
+            width: '80%',
             backgroundColor: '#419ED7',
             alignItems: 'center',
             justifyContent: 'center',
+            alignSelf: 'flex-start',
             height: 28,
             borderWidth: 1,
             borderColor: '#419ED7',
@@ -95,8 +98,7 @@ const EditarVacina_sty = StyleSheet.create({
         },
 
         button2: {
-            marginTop: 45,
-            width: '40%',
+            // marginTop: 45,
             height: 10,
             backgroundColor: '#37BD6D',
             height: 38,
@@ -108,7 +110,7 @@ const EditarVacina_sty = StyleSheet.create({
         },
 
         button3: {
-            marginTop: 150,
+            //  marginTop: 150,
             width: '25%',
             height: 10,
             backgroundColor: '#FD7979',
@@ -123,10 +125,155 @@ const EditarVacina_sty = StyleSheet.create({
     },
 
     trashIcon: {
-        width: '25%',
-        marginLeft: 0,
-        marginRight: 0,
-    }
+        width: 20,
+        height: 20,
+        marginLeft: 5,
+    },
+
+    containerDate: {
+        flex: 50,
+        alignSelf: 'flex-start',
+        flexDirection: 'row',
+        backgroundColor: '#FFFFFF',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        marginRight: 42,
+    },
+
+    dateIcon: {
+        width: 22,
+        height: 22,
+        marginRight: 5,
+    },
+
+    proofVaccineImage: {
+        width: 210,
+        height: 100,
+        alignSelf: 'flex-start',
+    },
+
+    textRadioButtonContainer: {
+        flex: 50,
+        alignItems: 'flex-end',
+        alignSelf: 'flex-start',
+        paddingTop: 7,
+    },
+
+    textRadioButton: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontFamily: 'AveriaLibre-Regular',
+    },
+
+    buttonsGroup: {
+        flex: 70,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: -8,
+    },
+
+    radioButton: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexBasis: '45%',
+    },
+
+    containerButtons: {
+        width: '100%',
+        height: '100%',
+        marginTop: 40,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 100,
+
+        button1: {
+            flex: 1,
+            width: '40%',
+            height: 35,
+            backgroundColor: '#37BD6D',
+            justifyContent: 'center',
+            alignItems: 'center',
+            elevation: 10,
+        },
+
+        button2: {
+            flex: 1,
+            flexDirection: 'row',
+            width: '25%',
+            height: 30,
+            backgroundColor: '#FD7979',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            gap: 5,
+            elevation: 10,
+        },
+
+
+    },
+
+    modalStyle: {
+        containerView: {
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.2)'
+        },
+
+        containerModal: {
+            flex: 1,
+            backgroundColor: '#FFFFFF',
+            justifyContent: 'space-between',
+            marginVertical: 330,
+            width: '70%',
+            borderColor: '#B9DFDB',
+            borderWidth: 2,
+            borderStyle: 'solid',
+        },
+
+        textContainer: {
+            flex: 1,
+            justifyContent: 'center',
+        },
+
+        modalText: {
+            color: '#FD7979',
+            fontSize: 19,
+            fontFamily: 'AveriaLibre-Regular',
+            textAlign: 'center',
+        },
+
+        buttonsContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 8,
+        },
+
+        buttonYes: {
+            width: '45%',
+            height: 35,
+            backgroundColor: '#FF8383',
+            justifyContent: 'center',
+        },
+
+        buttonCancel: {
+            width: '45%',
+            height: 35,
+            backgroundColor: '#3F92C5',
+            justifyContent: 'center',
+        },
+
+        buttonText: {
+            color: '#FFFFFF',
+            fontSize: 22,
+            fontFamily: 'AveriaLibre-Regular',
+            textAlign: 'center',
+        },
+    },
 
 })
 
--- projeto/src/screens/EditarVacina.js
+++ projeto/src/screens/EditarVacina.js
@@ -1,99 +1,142 @@
-import { View, Text, TouchableOpacity, TextInput, ScrollView, Image } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, ScrollView, Image, Modal } from "react-native";
 import { useState, useEffect } from 'react';
 import { KeyboardAvoidingView } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 
 import { EditarVacina_sty } from "../components/MyStyles/EditarVacina_sty";
 
-// TODO: Colocar icone de data.
+import contas from "../data/Contas";
+
 
 const EditarVacina = (props) => {
     const [dataVacinacao, setDataVacinacao] = useState(props.route.params?.data);
     const [vacina, setVacina] = useState(props.route.params?.nome);
     const [dose, setDose] = useState(props.route.params?.dose);
     const [proxVacinacao, setProxVacinacao] = useState(props.route.params?.proxVacinacao);
+    const [isVisible, setIsVisible] = useState(false);
 
+    const emailUsuarioLogado = props.route.params?.emailUsuarioLogado;
+
+    const modalYes = () => {
+        setIsVisible(false);
+        console.log(contas[emailUsuarioLogado]);
+        delete contas[emailUsuarioLogado].vacinas[vacina];
+        console.log(contas[emailUsuarioLogado].vacinas);
+        props.navigation.pop();
+        props.navigation.pop();
+        props.navigation.navigate("MyDrawer", { emailUsuarioLogado: emailUsuarioLogado });
+    };
+
+    const modalCancel = () => {
+        setIsVisible(false);
+    };
 
     return (
         <KeyboardAvoidingView style={EditarVacina_sty.container.containerKeyboard}>
             <ScrollView>
+                <Modal
+                    visible={isVisible}
+                    animationType='fade'
+                    transparent={true}>
+
+                    <View style={EditarVacina_sty.modalStyle.containerView}>
+                        <View style={EditarVacina_sty.modalStyle.containerModal}>
+                            <View style={EditarVacina_sty.modalStyle.textContainer} >
+                                <Text style={EditarVacina_sty.modalStyle.modalText}>Tem certeza que deseja remover essa vacina?</Text>
+                            </View>
+
+                            <View style={EditarVacina_sty.modalStyle.buttonsContainer}>
+                                <TouchableOpacity style={EditarVacina_sty.modalStyle.buttonYes} onPress={modalYes}>
+                                    <Text style={EditarVacina_sty.modalStyle.buttonText}>SIM</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={EditarVacina_sty.modalStyle.buttonCancel} onPress={modalCancel}>
+                                    <Text style={EditarVacina_sty.modalStyle.buttonText}>CANCELAR</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+
                 <View style={EditarVacina_sty.container.containerView}>
                     <View style={EditarVacina_sty.containerInputs}>
-                        <Text style={EditarVacina_sty.text}>Data de Vacinação</Text>
-                        <TextInput label={'DataVacinacao'} style={EditarVacina_sty.inputs} value={dataVacinacao} onChangeText={setDataVacinacao}></TextInput>
+                        <Text style={EditarVacina_sty.text}>Data de vacinação</Text>
+                        <View style={EditarVacina_sty.containerDate}>
+                            <TextInput label={'DataVacinacao'} style={EditarVacina_sty.inputs} value={dataVacinacao} onChangeText={setDataVacinacao}></TextInput>
+                            <Image source={require('../../assets/images/icon.png')} style={EditarVacina_sty.dateIcon} />
+                        </View>
                     </View>
                     <View style={EditarVacina_sty.containerInputs}>
                         <Text style={EditarVacina_sty.text}>Vacina</Text>
                         <TextInput label={'Vacina'} style={EditarVacina_sty.inputs} value={vacina} onChangeText={setVacina}></TextInput>
                     </View>
                     <View style={EditarVacina_sty.containerRadioButton}>
-                        <View style={{}}>
-                            <Text style={{
-                                color: '#FFFFFF',
-                                fontSize: 16,
-                                fontFamily: 'AveriaLibre-Regular',
-                                marginLeft: 68,
-                                paddingRight: 5,
-                            }}>Dose</Text>
+                        <View style={EditarVacina_sty.textRadioButtonContainer}>
+                            <Text style={EditarVacina_sty.textRadioButton}>Dose</Text>
                         </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <RadioButton
-                                value="dose1"
-                                status={dose == '1a. dose' ? 'checked' : 'unchecked'}
-                                onPress={() => setDose('1a. dose')}
-                                color="#419ED7"
-                            />
-                            <Text style={EditarVacina_sty.textRadioButton}>1a. dose</Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', paddingRight: 0 }}>
-                            <RadioButton
-                                value="dose2"
-                                status={dose == '2a. dose' ? 'checked' : 'unchecked'}
-                                onPress={() => setDose('2a. dose')}
-                                color="#419ED7"
-                            />
-                            <Text style={EditarVacina_sty.textRadioButton}>2a. dose</Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 127 }}>
-                            <RadioButton
-                                value="dose3"
-                                status={dose == '3a. dose' ? 'checked' : 'unchecked'}
-                                onPress={() => setDose('3a. dose')}
-                                color="#419ED7"
-                            />
-                            <Text style={EditarVacina_sty.textRadioButton}>3a. dose</Text>
-                        </View>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <RadioButton
-                                value="doseUnica"
-                                status={dose == 'Dose única' ? 'checked' : 'unchecked'}
-                                onPress={() => setDose('Dose única')}
-                                color="#419ED7"
-                            />
-                            <Text style={EditarVacina_sty.textRadioButton}>Dose única</Text>
+                        <View style={EditarVacina_sty.buttonsGroup}>
+                            <View style={EditarVacina_sty.radioButton}>
+                                <RadioButton
+                                    value="dose1"
+                                    status={dose == '1a. dose' ? 'checked' : 'unchecked'}
+                                    onPress={() => setDose('1a. dose')}
+                                    color="#419ED7"
+                                />
+                                <Text style={EditarVacina_sty.textRadioButton}>1a. dose</Text>
+                            </View>
+                            <View style={EditarVacina_sty.radioButton}>
+                                <RadioButton
+                                    value="dose2"
+                                    status={dose == '2a. dose' ? 'checked' : 'unchecked'}
+                                    onPress={() => setDose('2a. dose')}
+                                    color="#419ED7"
+                                />
+                                <Text style={EditarVacina_sty.textRadioButton}>2a. dose</Text>
+                            </View>
+                            <View style={EditarVacina_sty.radioButton}>
+                                <RadioButton
+                                    value="dose3"
+                                    status={dose == '3a. dose' ? 'checked' : 'unchecked'}
+                                    onPress={() => setDose('3a. dose')}
+                                    color="#419ED7"
+                                />
+                                <Text style={EditarVacina_sty.textRadioButton}>3a. dose</Text>
+                            </View>
+                            <View style={EditarVacina_sty.radioButton}>
+                                <RadioButton
+                                    value="doseUnica"
+                                    status={dose == 'Dose única' ? 'checked' : 'unchecked'}
+                                    onPress={() => setDose('Dose única')}
+                                    color="#419ED7"
+                                />
+                                <Text style={EditarVacina_sty.textRadioButton}>Dose única</Text>
+                            </View>
                         </View>
                     </View>
                     <View style={EditarVacina_sty.containerInputs}>
-                        <Text style={[EditarVacina_sty.text, { marginRight: 10, alignSelf: 'flex-start', marginTop: 3 }]}>Comprovante</Text>
+                        <Text style={[EditarVacina_sty.text, { alignSelf: 'flex-start', paddingTop: 2 }]}>Comprovante</Text>
                         <View style={EditarVacina_sty.buttons.buttonContainer}>
                             <TouchableOpacity style={EditarVacina_sty.buttons.button1}><Text style={EditarVacina_sty.buttons.textButton}>Selecionar imagem...</Text></TouchableOpacity>
-                            <Image source={require('../../assets/images/image-comprovante.png')} style={{ width: '100%', marginBottom: -25 }} resizeMode="contain"></Image>
+                            <Image source={require('../../assets/images/image-comprovante.png')} style={EditarVacina_sty.proofVaccineImage}></Image>
                         </View>
                     </View>
-                    <View style={EditarVacina_sty.containerInputs}>
+                    <View style={[EditarVacina_sty.containerInputs, { marginRight: 5 }]}>
                         <Text style={EditarVacina_sty.text}>Próxima vacinação</Text>
-                        <TextInput label={'ProximaVacina'} style={EditarVacina_sty.inputs} value={proxVacinacao} onChangeText={setProxVacinacao}></TextInput>
+                        <View style={EditarVacina_sty.containerDate}>
+                            <TextInput label={'ProximaVacinacao'} style={EditarVacina_sty.inputs} value={proxVacinacao} onChangeText={setProxVacinacao}></TextInput>
+                            <Image source={require('../../assets/images/icon.png')} style={EditarVacina_sty.dateIcon} />
+                        </View>
                     </View>
-                    <View style={EditarVacina_sty.buttons.button2}>
-                        <TouchableOpacity><Text style={EditarVacina_sty.buttons.textButton}>Salvar alterações</Text></TouchableOpacity>
-                    </View>
-                    <View style={EditarVacina_sty.buttons.button3}>
-                        <TouchableOpacity>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 8 }}>
-                                <Image source={require('../../assets/images/trash2.png')} style={EditarVacina_sty.trashIcon} resizeMode="contain" />
-                                <Text style={EditarVacina_sty.buttons.textButton}>Excluir</Text>
-                            </View>
+                    <View style={EditarVacina_sty.containerButtons}>
+
+                        <TouchableOpacity style={EditarVacina_sty.containerButtons.button1}>
+                            <Text style={EditarVacina_sty.buttons.textButton}>Salvar alterações</Text>
                         </TouchableOpacity>
+
+                        <TouchableOpacity style={EditarVacina_sty.containerButtons.button2} onPress={() => setIsVisible(true)}>
+                            <Image source={require('../../assets/images/trash2.png')} style={EditarVacina_sty.trashIcon} />
+                            <Text style={EditarVacina_sty.buttons.textButton}>Excluir</Text>
+                        </TouchableOpacity>
+
                     </View>
 
                 </View>
