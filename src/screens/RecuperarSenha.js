67a431470b4d1666ea748d1946449de75876859d
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-06-21 17:31:40 -0300

    Redefinição de senha implementado.

--- src/screens/RecuperarSenha.js
+++ src/screens/RecuperarSenha.js
@@ -3,8 +3,22 @@ import { useState } from 'react';
 
 import { RecuperarSenha_sty } from "../components/MyStyles/RecuperarSenha_sty";
 
+import { auth } from "../firebase/config";
+import { sendPasswordResetEmail } from 'firebase/auth';
+
 const RecuperarSenha = (props) => {
     const [email, setEmail] = useState('');
+
+    const recoverPassword = () => {
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                console.log("E-mail de redefinição enviado com sucesso. Verifique sua caixa de entrada")
+                props.navigation.popToTop();
+            })
+            .catch((error) => {
+                console.log("Falha ao enviar e-mail de redefinição: " + JSON.stringify(error))
+            })
+    }
 
     return (
         <KeyboardAvoidingView style={RecuperarSenha_sty.containerView.keyboard} behavior="height">
@@ -14,7 +28,7 @@ const RecuperarSenha = (props) => {
                     <TextInput placeholder="Seu e-mail..." placeholderTextColor={'#8B8B8B'} style={RecuperarSenha_sty.input} label={'Email'} value={email} onChangeText={setEmail}></TextInput>
                 </View>
 
-                <TouchableOpacity style={RecuperarSenha_sty.button} onPress={() => props.navigation.popToTop()}>
+                <TouchableOpacity style={RecuperarSenha_sty.button} onPress={recoverPassword}>
                     <Text style={RecuperarSenha_sty.textButton}>Recuperar senha</Text>
                 </TouchableOpacity>
             </View>
